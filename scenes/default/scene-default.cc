6da9212b366244042eeded0b287f6a215c67676b
Theo Lepage <theo.lepage@epita.fr>
2021-07-10 00:13:30 +0200

    add a sphere to scene-default

--- scenes/default/scene-default.cc
+++ scenes/default/scene-default.cc
@@ -15,11 +15,9 @@ constexpr float LIGHT_POS[] = {-1.f, 1.f, 2.f, 1.f};
 constexpr int NB_CUBES = 2;
 
 constexpr glm::vec3 CUBE_POS[] = {
-    glm::vec3(0.0f, -1.0f, -2.0f),
+    glm::vec3(0.0f, -1.5f, -2.0f),
     glm::vec3(3.0f, -2.0f, -6.0f)
 };
-
-std::size_t nb_vertices;
 
 void initUniformVariables()
 {
@@ -28,7 +26,7 @@ void initUniformVariables()
         glUniform4f(loc, LIGHT_POS[0], LIGHT_POS[1], LIGHT_POS[2], LIGHT_POS[3]);
 }
 
-void initVAO()
+int initVAO(const std::string& obj_path)
 {
     GLuint vao_id;
     GLuint vbo_vertex_ids[3];
@@ -36,8 +34,7 @@ void initVAO()
     std::vector<glm::vec3> vertices;
     std::vector<glm::vec2> uvs;
     std::vector<glm::vec3> normals;
-    mygl::ObjLoader::load_obj("../scenes/default/cube.obj", vertices, uvs, normals);
-    nb_vertices = vertices.size();
+    mygl::ObjLoader::load_obj(obj_path, vertices, uvs, normals);
 
     glGenVertexArrays(1, &vao_id);
     glBindVertexArray(vao_id);
@@ -58,13 +55,17 @@ void initVAO()
     glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(glm::vec3), &normals[0], GL_STATIC_DRAW);
     glEnableVertexAttribArray(2);
     glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+    return vertices.size();
 }
 
 void display()
 {
-    glClearColor(0, 0, 0, 0);
+    glClearColor(0.756, 0.854, 0.984, 1);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
+
+    // Draw cubes
+    int nb_vertices = initVAO("../scenes/default/cube.obj");
     for (unsigned int i = 0; i < NB_CUBES; i++)
     {
         // Translate cube
@@ -83,6 +84,15 @@ void display()
         glDrawArrays(GL_TRIANGLES, 0, nb_vertices);
     }
 
+    // Draw sphere
+    nb_vertices = initVAO("../scenes/default/sphere.obj");
+    glm::mat4 world_to_cam_matrix = mygl::Camera::get_instance()->get_world_to_cam_matrix();
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+    world_to_cam_matrix = world_to_cam_matrix * model;
+    glUniformMatrix4fv(glGetUniformLocation(mygl::Program::get_instance()->get_id(), "world_to_cam_matrix"), 1, GL_FALSE, &world_to_cam_matrix[0][0]);
+    glDrawArrays(GL_TRIANGLES, 0, nb_vertices);
+
     glutSwapBuffers();
 }
 
@@ -92,7 +102,6 @@ int main(int argc, char* argv[])
     glutDisplayFunc(display);
 
     initUniformVariables();
-    initVAO();
 
     TextureManager texture_manager(program);
     texture_manager.add_texture("../scenes/default/texture.png", "texture_sampler");
