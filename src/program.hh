921e7280b02acc481b929bec7b9c013d964f7f2f
Quentin Kaci <quentin.kaci@epita.fr>
2021-07-15 22:28:43 +0200

    refactor program

--- src/program.hh
+++ src/program.hh
@@ -30,81 +30,83 @@ namespace mygl
 
         inline void add_program(const std::string& vertex_shader_src, const std::string& fragment_shader_src)
         {
+            // Code adapted from https://www.khronos.org/opengl/wiki/Shader_Compilation
+
             size_t nb_programs = programs_.size();
 
             singleton_->programs_.push_back(-1);
 
-            GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+            GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
 
             const auto* source = (const GLchar*)vertex_shader_src.c_str();
-            glShaderSource(vertexShader, 1, &source, 0);
+            glShaderSource(vertex_shader, 1, &source, 0);
 
-            glCompileShader(vertexShader);
+            glCompileShader(vertex_shader);
 
-            GLint isCompiled = 0;
-            glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isCompiled);
-            if (isCompiled == GL_FALSE)
+            GLint is_compiled = 0;
+            glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &is_compiled);
+            if (is_compiled == GL_FALSE)
             {
-                GLint maxLength = 0;
-                glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
+                GLint max_length = 0;
+                glGetShaderiv(vertex_shader, GL_INFO_LOG_LENGTH, &max_length);
 
-                std::vector<GLchar> infoLog(maxLength);
-                glGetShaderInfoLog(vertexShader, maxLength, &maxLength, &infoLog[0]);
+                std::vector<GLchar> info_log(max_length);
+                glGetShaderInfoLog(vertex_shader, max_length, &max_length, &info_log[0]);
 
-                glDeleteShader(vertexShader);
+                glDeleteShader(vertex_shader);
 
-                singleton_->last_log = &infoLog[0];
+                singleton_->last_log = &info_log[0];
             }
 
-            GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+            GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
 
             source = (const GLchar*)fragment_shader_src.c_str();
-            glShaderSource(fragmentShader, 1, &source, 0);
+            glShaderSource(fragment_shader, 1, &source, 0);
 
-            glCompileShader(fragmentShader);
+            glCompileShader(fragment_shader);
 
-            glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isCompiled);
-            if (isCompiled == GL_FALSE)
+            glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &is_compiled);
+            if (is_compiled == GL_FALSE)
             {
-                GLint maxLength = 0;
-                glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &maxLength);
+                GLint max_length = 0;
+                glGetShaderiv(fragment_shader, GL_INFO_LOG_LENGTH, &max_length);
 
-                std::vector<GLchar> infoLog(maxLength);
-                glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, &infoLog[0]);
+                std::vector<GLchar> info_log(max_length);
+                glGetShaderInfoLog(fragment_shader, max_length, &max_length, &info_log[0]);
 
-                glDeleteShader(fragmentShader);
-                glDeleteShader(vertexShader);
+                glDeleteShader(fragment_shader);
+                glDeleteShader(vertex_shader);
 
-                singleton_->last_log = &infoLog[0];
+                singleton_->last_log = &info_log[0];
             }
 
             GLuint program = glCreateProgram();
 
             singleton_->programs_[nb_programs] = program;
 
-            glAttachShader(program, vertexShader);
-            glAttachShader(program, fragmentShader);
+            glAttachShader(program, vertex_shader);
+            glAttachShader(program, fragment_shader);
             glLinkProgram(program);
 
-            GLint isLinked = 0;
-            glGetProgramiv(program, GL_LINK_STATUS, (int*)&isLinked);
-            if (isLinked == GL_FALSE)
+            GLint is_linked = 0;
+            glGetProgramiv(program, GL_LINK_STATUS, (int*)&is_linked);
+            if (is_linked == GL_FALSE)
             {
-                GLint maxLength = 0;
-                glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+                GLint max_length = 0;
+                glGetProgramiv(program, GL_INFO_LOG_LENGTH, &max_length);
 
-                std::vector<GLchar> infoLog(maxLength);
-                glGetProgramInfoLog(program, maxLength, &maxLength, &infoLog[0]);
+                std::vector<GLchar> info_log(max_length);
+                glGetProgramInfoLog(program, max_length, &max_length, &info_log[0]);
 
                 glDeleteProgram(program);
-                glDeleteShader(vertexShader);
-                glDeleteShader(fragmentShader);
+                glDeleteShader(vertex_shader);
+                glDeleteShader(fragment_shader);
 
-                singleton_->last_log = &infoLog[0];
+                singleton_->last_log = &info_log[0];
             }
 
-            glDetachShader(program, vertexShader);
-            glDetachShader(program, fragmentShader);
+            glDetachShader(program, vertex_shader);
+            glDetachShader(program, fragment_shader);
 
             singleton_->is_last_ready_ = true;
         };
